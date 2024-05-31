9a1144c99fc43189856aecbf3de9049da72409cf
Yuichiro Okashita <yu_okashita@krd.biglobe.ne.jp>
2024-05-31 13:27:49 +0900

    プロジェクト ファイルを追加します。

---
+++ Kalmia_OpenLab/View/Animator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalmia_OpenLab.View
+{
+    internal class Animator
+    {
+        public event EventHandler OnEndAnimation = delegate { };
+
+        Func<int, int, bool> callback;
+        bool stopFlag;
+
+        public Animator(Func<int, int, bool> callback) => this.callback = callback;
+
+        public void AnimateFornumFrames(double interval, int numFrames)
+        {
+            this.stopFlag = false;
+
+            Task.Run(() =>
+            {
+                var nextTiming = (double)Environment.TickCount;
+                for (var frameCount = 0; !this.stopFlag && frameCount < numFrames; frameCount++)
+                {
+                    while (Environment.TickCount < nextTiming) ;
+                    if (!this.callback(frameCount, numFrames))
+                        break;
+                    nextTiming += interval;
+                }
+                this.OnEndAnimation.Invoke(this, EventArgs.Empty);
+            });
+        }
+
+        public void AnimateForDuration(double interval, int duration)
+        {
+            if (duration < interval)
+                duration = (int)interval;
+            AnimateFornumFrames(interval, (int)(duration / interval));
+        }
+
+        public void Stop() => this.stopFlag = true;
+    }
+}
